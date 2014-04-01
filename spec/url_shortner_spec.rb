be0237b08d2c01460e500530c74d6152f30a0f4c
Cara Dennis <Cara@https://github.com/ephemere>
2014-04-01 15:55:40 -0600

    [#68381636] User visits URL and is redirected
    Signed-off-by: Zach Klabunde <zbunde@gmail.com>

--- spec/url_shortner_spec.rb
+++ spec/url_shortner_spec.rb
@@ -11,8 +11,10 @@ feature 'UrlShortener' do
     click_on('Shorten')
     expect(page).to have_content('http://wwww.example.com/1')
     visit '/'
-    fill_in 'urlText', with: 'whatever.com'
+    fill_in 'urlText', with: 'http://www.google.com'
     click_on('Shorten')
     expect(page).to have_content('http://wwww.example.com/2')
+    click_on('short')
+    page.has_xpath?('google.com')
   end
 end
